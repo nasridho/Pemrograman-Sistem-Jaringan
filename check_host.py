d2ac498a00fb07396ef4eefa1e1740d6587944ce
nasridho <rmsyfa@gmail.com>
2021-03-16 22:58:22 +0700

    initial

---
+++ check_host.py
@@ -0,0 +1,19 @@
+import subprocess
+import sys
+
+argv_len = len(sys.argv[1:])
+if not argv_len==1:
+	sys.exit(f'Gagal: IP address belum diberikan')
+
+
+ipnya = sys.argv[1:]
+ip = ipnya[0]
+
+p = subprocess.Popen('ping '+ip,stdout=subprocess.PIPE)
+
+p.wait()
+if p.poll():
+    print ("Host " + ip + " is DOWN")
+else:
+    print ("Host " + ip+" is UP")
+
